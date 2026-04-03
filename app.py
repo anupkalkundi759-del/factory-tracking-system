314902ff6eda85513a3f173ef76c76099faaeed6
anupkalkundi759-del <anupkalkundi759@gmail.com>
2026-04-03 17:44:45 +0530

    app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if not st.session_state.logged_in:
 # ==============================
 try:
     conn = psycopg2.connect(
-        "postgresql://postgres.veiqtpgsiarxboikevgk:Temc2266%23%2A%24@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+    "postgresql://postgres.veiqtpgsiarxboikevgk:Temc2266#*$@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
     )
     cur = conn.cursor()
 
